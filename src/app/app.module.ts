c3e8d1601c1b56d6f43c35844ccf88e791e5ec81
unknown <finfokka@gmail.com>
2022-10-10 15:49:15 -0300

    primeira versão com o menu principal montado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { QuizzComponent } from './pages/quizz/quizz.component';
+import { OptionComponent } from './components/option/option.component';
+import { LoadingComponent } from './pages/loading/loading.component';
+import { ResultComponent } from './pages/result/result.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { QuizzCardComponent } from './components/quizz-card/quizz-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuizzComponent,
+    OptionComponent,
+    LoadingComponent,
+    ResultComponent,
+    MenubarComponent,
+    QuizzCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/quizz-card/quizz-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz-card',
+  templateUrl: './quizz-card.component.html',
+  styleUrls: ['./quizz-card.component.css']
+})
+export class QuizzCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
