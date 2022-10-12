c14cb6ec80cd84d4c9c910dcf2e2f0f32c12aed2
unknown <finfokka@gmail.com>
2022-10-12 19:12:09 -0300

    Progresso na pagina de quizz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { QuizzComponent } from './pages/quizz/quizz.component';
 
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'quizz', component: QuizzComponent}
 
 ];
 
